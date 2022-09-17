e07a16d8c7ac200800daa0236db71e9088fff462
Ts3ngo <101639555+Tsengo@users.noreply.github.com>
2022-09-18 01:25:21 +0400

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,11 @@
+// const navBar = ["Product","Services","About","Log In"];
+
+// let list = "<ul>";
+//     for(let i = 0; i < navBar.length; i++){
+//         list += '<li><a href="#">'+navBar[i]+'</a></li>';
+//     }
+// list += "</ul>";
+
+// console.log(list);
+// document.getElementById("navbar").innerHTML = list;
+
